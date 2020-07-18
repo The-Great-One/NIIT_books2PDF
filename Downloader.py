4628cf6fb4a056ea36b767e7ebe399a8ed0a9f5a
Sahil Goel <sahilgoel187@gmail.com>
2020-07-19 00:22:58 +0530

    Fixed Sorting and re-enabled menu

--- Downloader.py
+++ Downloader.py
@@ -4,17 +4,12 @@ from urllib.request import urlopen
 import os
 import canvas as canvas
 from bs4 import BeautifulSoup
-url="http://courseware.training.com/ResourceReader/RadPdf.axd?rt=3&dk=0002D09E3226RWXHAG505BOFK88ETVKOB&pn="
-        #input("Enter Url for the book you want(Without 1&pit=2): ")
-pages=276
-        #int(input("Enter the pages in the book: "))
-dire="D:\\Desktop\\book\\book"
-    #input("Enter Directory to be saved in with the file name(EX: C:\\MyDirectory\\FileName): ")
-
+url=input("Enter Url for the book you want(Without 1&pit=2): ")
+pages=int(input("Enter the pages in the book: "))
+dire=input("Enter Directory to be saved in with the file name(EX: C:\\MyDirectory\\FileName): ")
 
 count=1
 for i in range(1,pages+1):
-        #resource=urlopen("http://courseware.training.com/ResourceReader/RadPdf.axd?rt=3&dk=0002D09E3226RWXHAG505BOFK88ETVKOB&pn="+str(i)+"&pit=2")
         resource=urlopen(url+str(i)+"&pit=2")
         output=open(dire+str(count)+".jpg","wb")
         output.write(resource.read())
@@ -33,6 +28,7 @@ with open("book.pdf","wb") as f:
 		if os.path.isdir(path):
 			continue
 		imgs.append(path)
+	imgs.sort(key=len)
 	f.write(img2pdf.convert(imgs))
 	print("PDF Created")
 print("Done")
